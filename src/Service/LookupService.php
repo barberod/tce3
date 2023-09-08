1ed3b4a0bf14bcf3a4e0e1127cb4ed21409d7289
David Barbero <barberod@outlook.com>
2023-09-08 16:19:53 -0400

    more ldap capitalization

--- src/Service/LookupService.php
+++ src/Service/LookupService.php
@@ -189,7 +189,7 @@ class LookupService
             return true;
         }
         if (
-            (in_array("celster3", $entry->getAttribute('gtPrimaryGtAccountUsername')))
+            (in_array("celster3", $entry->getAttribute('gtPrimaryGTAccountUsername')))
         ) {
             return true;
         }
@@ -198,7 +198,7 @@ class LookupService
 
     private function isAdmin(Entry $entry): bool {
         if (
-            (in_array("dbarbero3", $entry->getAttribute('gtPrimaryGtAccountUsername')))
+            (in_array("dbarbero3", $entry->getAttribute('gtPrimaryGTAccountUsername')))
         ) {
             return true;
         }
