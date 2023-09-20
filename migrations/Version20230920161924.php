09de9f750537e06b179a10e69867099e9c01fdfa
David Barbero <barberod@outlook.com>
2023-09-20 16:25:05 -0400

    create two database migrations

---
+++ migrations/Version20230920161924.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230920161924 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE institution (id INT AUTO_INCREMENT NOT NULL, dapip_id VARCHAR(9) DEFAULT NULL, usg_id VARCHAR(9) DEFAULT NULL, name VARCHAR(90) DEFAULT NULL, state VARCHAR(2) DEFAULT NULL, address VARCHAR(255) DEFAULT NULL, status INT NOT NULL, d7_nid INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE institution');
+    }
+}
