e24ff575c4e80e492c68d996c17e1d74817bba1a
David Barbero <barberod@outlook.com>
2023-11-01 14:19:51 -0400

    add evaluation voter to manage access

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Evaluation;
 use App\Entity\User;
 use App\Repository\EvaluationRepository;
+use App\Service\EvaluationOptionsService;
 use Doctrine\ORM\EntityManagerInterface;
 use EcPhp\CasBundle\Security\Core\User\CasUser;
 use Pagerfanta\Doctrine\ORM\QueryAdapter;
@@ -15,12 +16,17 @@ use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class PageController extends AbstractController
 {
+		private EntityManagerInterface $entityManager;
+
     public function __construct(
         EntityManagerInterface $entityManager
-    ) {}
+    ) {
+				$this->entityManager = $entityManager;
+		}
 
     #[Route('/', name: 'homepage')]
     public function index(): Response
@@ -84,7 +90,8 @@ class PageController extends AbstractController
         ]);
     }
 
-    #[Route('/secure/coordinator/evaluation', name: 'coordinator_evaluation_table', methods: ['GET'])]
+    #[Route('/secure/coordinator/evaluation',
+			name: 'coordinator_evaluation_table', methods: ['GET'])]
     public function coordinatorEvaluationTable(EvaluationRepository $evaluationRepository): Response
     {
         $page = (isset($_GET['page']) && is_numeric($_GET['page'])) ? $_GET['page'] : 1;
@@ -103,32 +110,399 @@ class PageController extends AbstractController
         ]);
     }
 
-    #[Route('/secure/coordinator/evaluation/{id}', name: 'coordinator_evaluation_page', methods: ['GET'])]
+    #[Route('/secure/coordinator/evaluation/{id}',
+			name: 'coordinator_evaluation_page', methods: ['GET'])]
+		#[IsGranted('READ', 'evaluation')]
     public function coordinatorEvaluationPage(Evaluation $evaluation): Response
     {
-        return $this->render('evaluation/page.html.twig', [
+				$optionsService = new EvaluationOptionsService($this->entityManager);
+				return $this->render('evaluation/page.html.twig', [
             'context' => 'coordinator',
             'page_title' => 'Evaluation #'.$evaluation->getID(),
             'prepend' => 'Evaluation #'.$evaluation->getID(),
             'evaluation' => $evaluation,
             'id' => $evaluation->getID(),
 						'uuid' => $evaluation->getID(),
+						'options' => $optionsService->getOptions('coordinator', $evaluation, $this->getUser()),
         ]);
     }
 
-    #[Route('/secure/coordinator/evaluation/{id}/{verb}', name: 'coordinator_evaluation_form', methods: ['GET'])]
-    public function coordinatorEvaluationForm(Evaluation $evaluation, string $verb): Response
+    #[Route('/secure/coordinator/evaluation/{id}/update',
+			name: 'coordinator_evaluation_update_form', methods: ['GET', 'POST'])]
+		#[IsGranted('UPDATE', 'evaluation')]
+    public function coordinatorEvaluationUpdateForm(Evaluation $evaluation): Response
     {
         return $this->render('evaluation/page.html.twig', [
 						'context' => 'coordinator',
 						'page_title' => 'Evaluation #'.$evaluation->getID(),
-						'prepend' => 'Evaluation #'.$evaluation->getID(),
+						'prepend' => 'Edit Details | Evaluation #'.$evaluation->getID(),
 						'evaluation' => $evaluation,
 						'id' => $evaluation->getID(),
 						'uuid' => $evaluation->getID(),
-            'verb' => $verb
-        ]);
+						'verb' => 'update'
+				]);
     }
+
+		#[Route('/secure/coordinator/evaluation/{id}/delete',
+			name: 'coordinator_evaluation_delete_form', methods: ['GET', 'POST'])]
+		#[IsGranted('DELETE', 'evaluation')]
+		public function coordinatorEvaluationDeleteForm(Evaluation $evaluation): Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'coordinator',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Delete | Evaluation #'.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'delete'
+				]);
+		}
+
+		#[Route('/secure/coordinator/evaluation/{id}/annotate',
+			name: 'coordinator_evaluation_annotate_form', methods: ['GET', 'POST'])]
+		#[IsGranted('ANNOTATE', 'evaluation')]
+		public function coordinatorEvaluationAnnotateForm(Evaluation $evaluation): Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'coordinator',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Write a Note | Evaluation #'.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'annotate'
+				]);
+		}
+
+		#[Route('/secure/coordinator/evaluation/{id}/annotate-as-requester',
+			name: 'coordinator_evaluation_annotate_as_requester_form', methods: ['GET', 'POST'])]
+		#[IsGranted('ANNOTATE_AS_REQUESTER', 'evaluation')]
+		public function coordinatorEvaluationAnnotateAsRequesterForm(Evaluation $evaluation): Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+						'context' => 'coordinator',
+						'page_title' => 'Evaluation #'.$evaluation->getID(),
+						'prepend' => 'Write a Note | Evaluation #'.$evaluation->getID(),
+						'evaluation' => $evaluation,
+						'id' => $evaluation->getID(),
+						'uuid' => $evaluation->getID(),
+						'verb' => 'annotate-as-requester'
+				]);
+		}
+
+		#[Route('/secure/coordinator/evaluation/{id}/append',
+			name: 'coordinator_evaluation_append_form', methods: ['GET', 'POST'])]
+		#[IsGranted('APPEND', 'evaluation')]
+		public function coordinatorEvaluationAppendForm(Evaluation $evaluation):
+		Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'coordinator',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Upload a File | Evaluation #'.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'append'
+				]);
+		}
+
+		#[Route('/secure/coordinator/evaluation/{id}/assign',
+			name: 'coordinator_evaluation_assign_form', methods: ['GET', 'POST'])]
+		#[IsGranted('ASSIGN', 'evaluation')]
+		public function coordinatorEvaluationAssignForm(Evaluation $evaluation):
+		Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'coordinator',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Assign to Department | Evaluation #'
+						.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'assign'
+				]);
+		}
+
+		#[Route('/secure/coordinator/evaluation/{id}/evaluate',
+			name: 'coordinator_evaluation_evaluate_form', methods: ['GET', 'POST'])]
+		#[IsGranted('EVALUATE', 'evaluation')]
+		public function coordinatorEvaluationEvaluateForm(Evaluation $evaluation):
+		Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'coordinator',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Enter Equivalencies | Evaluation #'
+						.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'evaluate'
+				]);
+		}
+
+		#[Route('/secure/coordinator/evaluation/{id}/example',
+			name: 'coordinator_evaluation_example_form', methods: ['GET', 'POST'])]
+		#[IsGranted('EXAMPLE', 'evaluation')]
+		public function coordinatorEvaluationExampleForm(Evaluation $evaluation):
+		Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'coordinator',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Example | Evaluation #'.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'example'
+				]);
+		}
+
+		#[Route('/secure/coordinator/evaluation/{id}/finalize',
+			name: 'coordinator_evaluation_finalize_form', methods: ['GET', 'POST'])]
+		#[IsGranted('FINALIZE', 'evaluation')]
+		public function coordinatorEvaluationFinalizeForm(Evaluation $evaluation):
+		Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'coordinator',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Finalize | Evaluation #'.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'finalize'
+				]);
+		}
+
+		#[Route('/secure/coordinator/evaluation/{id}/forward',
+			name: 'coordinator_evaluation_forward_form', methods: ['GET', 'POST'])]
+		#[IsGranted('FORWARD', 'evaluation')]
+		public function coordinatorEvaluationForwardForm(Evaluation $evaluation):
+		Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'coordinator',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Forward to a Colleague | Evaluation #'
+						.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'forward'
+				]);
+		}
+
+		#[Route('/secure/coordinator/evaluation/{id}/from-complete-to-hold',
+			name: 'coordinator_evaluation_from_complete_to_hold_form', methods: ['GET', 'POST'])]
+		#[IsGranted('FROM_COMPLETE_TO_HOLD', 'evaluation')]
+		public function coordinatorEvaluationFromCompleteToHoldForm(Evaluation $evaluation):	Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'coordinator',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Hold | Evaluation #'.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'from-complete-to-hold'
+				]);
+		}
+
+		#[Route('/secure/coordinator/evaluation/{id}/from-dept-to-r1',
+			name: 'coordinator_evaluation_from_dept_to_r1_form', methods: ['GET', 'POST'])]
+		#[IsGranted('FROM_DEPT_TO_R1', 'evaluation')]
+		public function coordinatorEvaluationFromDeptToR1Form(Evaluation $evaluation):	Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'coordinator',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Send to R1 | Evaluation #'.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'from-dept-to-r1'
+				]);
+		}
+
+		#[Route('/secure/coordinator/evaluation/{id}/from-dept-to-student', name:
+			'coordinator_evaluation_from_dept_to_student_form', methods: ['GET', 'POST'])]
+		#[IsGranted('FROM_DEPT_TO_STUDENT', 'evaluation')]
+		public function coordinatorEvaluationFromDeptToStudentForm(Evaluation $evaluation):	Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'coordinator',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Send to Student | Evaluation #'.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'from-dept-to-student'
+				]);
+		}
+
+		#[Route('/secure/coordinator/evaluation/{id}/from-r1-to-student', name:
+			'coordinator_evaluation_from_r1_to_student_form', methods: ['GET', 'POST'])]
+		#[IsGranted('FROM_R1_TO_STUDENT', 'evaluation')]
+		public function coordinatorEvaluationFromR1ToStudentForm(Evaluation $evaluation):	Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'coordinator',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Send to Student | Evaluation #'.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'from-r1-to-student'
+				]);
+		}
+
+		#[Route('/secure/coordinator/evaluation/{id}/from-r2-to-dept', name:
+			'coordinator_evaluation_from_r2_to_dept_form', methods: ['GET', 'POST'])]
+		#[IsGranted('FROM_R2_TO_DEPT', 'evaluation')]
+		public function coordinatorEvaluationFromR2ToDeptForm(Evaluation $evaluation):	Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'coordinator',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Send to Department | Evaluation #'.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'from-r2-to-dept'
+				]);
+		}
+
+		#[Route('/secure/coordinator/evaluation/{id}/from-r2-to-student', name:
+			'coordinator_evaluation_from_r2_to_student_form', methods: ['GET', 'POST'])]
+		#[IsGranted('FROM_R2_TO_STUDENT', 'evaluation')]
+		public function coordinatorEvaluationFromR2ToStudentForm(Evaluation $evaluation):	Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'coordinator',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Send to Student | Evaluation #'.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'from-r2-to-student'
+				]);
+		}
+
+		#[Route('/secure/coordinator/evaluation/{id}/from-student-to-r1', name:
+			'coordinator_evaluation_from_student_to_r1_form', methods: ['GET', 'POST'])]
+		#[IsGranted('FROM_STUDENT_TO_R1', 'evaluation')]
+		public function coordinatorEvaluationFromStudentToR1Form(Evaluation $evaluation):	Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'coordinator',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Send to R1 | Evaluation #'.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'from-student-to-r1'
+				]);
+		}
+
+		#[Route('/secure/coordinator/evaluation/{id}/hide',
+			name: 'coordinator_evaluation_hide_form', methods: ['GET', 'POST'])]
+		#[IsGranted('HIDE', 'evaluation')]
+		public function coordinatorEvaluationHideForm(Evaluation $evaluation): Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'coordinator',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Hide | Evaluation #'.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'hide'
+				]);
+		}
+
+		#[Route('/secure/coordinator/evaluation/{id}/hold',
+			name: 'coordinator_evaluation_hold_form', methods: ['GET', 'POST'])]
+		#[IsGranted('HOLD', 'evaluation')]
+		public function coordinatorEvaluationHoldForm(Evaluation $evaluation): Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+						'context' => 'coordinator',
+						'page_title' => 'Evaluation #'.$evaluation->getID(),
+						'prepend' => 'Hold | Evaluation #'.$evaluation->getID(),
+						'evaluation' => $evaluation,
+						'id' => $evaluation->getID(),
+						'uuid' => $evaluation->getID(),
+						'verb' => 'hold'
+				]);
+		}
+
+		#[Route('/secure/coordinator/evaluation/{id}/pass',
+			name: 'coordinator_evaluation_pass_form', methods: ['GET', 'POST'])]
+		#[IsGranted('PASS', 'evaluation')]
+		public function coordinatorEvaluationPassForm(Evaluation $evaluation): Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+						'context' => 'coordinator',
+						'page_title' => 'Evaluation #'.$evaluation->getID(),
+						'prepend' => 'Pass | Evaluation #'.$evaluation->getID(),
+						'evaluation' => $evaluation,
+						'id' => $evaluation->getID(),
+						'uuid' => $evaluation->getID(),
+						'verb' => 'pass'
+				]);
+		}
+
+		#[Route('/secure/coordinator/evaluation/{id}/reassign',
+			name: 'coordinator_evaluation_reassign_form', methods: ['GET', 'POST'])]
+		#[IsGranted('REASSIGN', 'evaluation')]
+		public function coordinatorEvaluationReassignForm(Evaluation $evaluation): Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+						'context' => 'coordinator',
+						'page_title' => 'Evaluation #'.$evaluation->getID(),
+						'prepend' => 'Reassign to a Department | Evaluation #'.$evaluation->getID(),
+						'evaluation' => $evaluation,
+						'id' => $evaluation->getID(),
+						'uuid' => $evaluation->getID(),
+						'verb' => 'reassign'
+				]);
+		}
+
+		#[Route('/secure/coordinator/evaluation/{id}/resubmit',
+			name: 'coordinator_evaluation_resubmit_form', methods: ['GET', 'POST'])]
+		#[IsGranted('RESUBMIT', 'evaluation')]
+		public function coordinatorEvaluationResubmitForm(Evaluation $evaluation): Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+						'context' => 'coordinator',
+						'page_title' => 'Evaluation #'.$evaluation->getID(),
+						'prepend' => 'Resubmit | Evaluation #'.$evaluation->getID(),
+						'evaluation' => $evaluation,
+						'id' => $evaluation->getID(),
+						'uuid' => $evaluation->getID(),
+						'verb' => 'resubmit'
+				]);
+		}
+
+		#[Route('/secure/coordinator/evaluation/{id}/spot-articulate',
+			name: 'coordinator_evaluation_spot_articulate_form', methods: ['GET', 'POST'])]
+		#[IsGranted('SPOT_ARTICULATE', 'evaluation')]
+		public function coordinatorEvaluationSpotArticulateForm(Evaluation $evaluation): Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+						'context' => 'coordinator',
+						'page_title' => 'Evaluation #'.$evaluation->getID(),
+						'prepend' => 'Spot Articulate | Evaluation #'.$evaluation->getID(),
+						'evaluation' => $evaluation,
+						'id' => $evaluation->getID(),
+						'uuid' => $evaluation->getID(),
+						'verb' => 'spot-articulate'
+				]);
+		}
 
     #[Route('/secure/coordinator/course', name: 'coordinator_course_table')]
     public function coordinatorCourseTable(): Response
---
+++ src/Security/Voter/EvaluationVoter.php
@@ -0,0 +1,229 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Evaluation;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class EvaluationVoter extends Voter
+{
+		public const CREATE = 'CREATE';
+		public const READ = 'READ';
+		public const UPDATE = 'UPDATE';
+		public const DELETE = 'DELETE';
+		public const ANNOTATE = 'ANNOTATE';
+		public const ANNOTATE_AS_REQUESTER = 'ANNOTATE_AS_REQUESTER';
+		public const APPEND = 'APPEND';
+		public const ASSIGN = 'ASSIGN';
+		public const EVALUATE = 'EVALUATE';
+		public const EXAMPLE = 'EXAMPLE';
+		public const FINALIZE = 'FINALIZE';
+		public const FORWARD = 'FORWARD';
+		public const FROM_COMPLETE_TO_HOLD = 'FROM_COMPLETE_TO_HOLD';
+		public const FROM_DEPT_TO_R1 = 'FROM_DEPT_TO_R1';
+		public const FROM_DEPT_TO_STUDENT = 'FROM_DEPT_TO_STUDENT';
+		public const FROM_R1_TO_STUDENT = 'FROM_R1_TO_STUDENT';
+		public const FROM_R2_TO_DEPT = 'FROM_R2_TO_DEPT';
+		public const FROM_R2_TO_STUDENT = 'FROM_R2_TO_STUDENT';
+		public const FROM_STUDENT_TO_R1 = 'FROM_STUDENT_TO_R1';
+		public const HIDE = 'HIDE';
+		public const HOLD = 'HOLD';
+		public const PASS = 'PASS';
+		public const REASSIGN = 'REASSIGN';
+		public const RESUBMIT = 'RESUBMIT';
+		public const SPOT_ARTICULATE = 'SPOT_ARTICULATE';
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+				$verbs = [
+						self::CREATE,
+						self::READ,
+						self::UPDATE,
+						self::DELETE,
+						self::ANNOTATE,
+						self::ANNOTATE_AS_REQUESTER,
+						self::APPEND,
+						self::ASSIGN,
+						self::EVALUATE,
+						self::EXAMPLE,
+						self::FINALIZE,
+						self::FORWARD,
+						self::FROM_COMPLETE_TO_HOLD,
+						self::FROM_DEPT_TO_R1,
+						self::FROM_DEPT_TO_STUDENT,
+						self::FROM_R1_TO_STUDENT,
+						self::FROM_R2_TO_DEPT,
+						self::FROM_R2_TO_STUDENT,
+						self::FROM_STUDENT_TO_R1,
+						self::HIDE,
+						self::HOLD,
+						self::PASS,
+						self::REASSIGN,
+						self::RESUBMIT,
+						self::SPOT_ARTICULATE
+				];
+
+        return in_array($attribute, $verbs)
+            && $subject instanceof \App\Entity\Evaluation;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+				return match ($attribute) {
+						self::CREATE => $this->canCreate($subject, $user),
+						self::READ => $this->canRead($subject, $user),
+						self::UPDATE => $this->canUpdate($subject, $user),
+						self::DELETE => $this->canDelete($subject, $user),
+						self::ANNOTATE => $this->canAnnotate($subject, $user),
+						self::ANNOTATE_AS_REQUESTER => $this->canAnnotateAsRequester(
+							$subject,
+							$user
+						),
+						self::APPEND => $this->canAppend($subject, $user),
+						self::ASSIGN => $this->canAssign($subject, $user),
+						self::EVALUATE => $this->canEvaluate($subject, $user),
+						self::EXAMPLE => $this->canExample($subject, $user),
+						self::FINALIZE => $this->canFinalize($subject, $user),
+						self::FORWARD => $this->canForward($subject, $user),
+						self::FROM_COMPLETE_TO_HOLD => $this->canFromCompleteToHold(
+							$subject,
+							$user
+						),
+						self::FROM_DEPT_TO_R1 => $this->canFromDeptToR1($subject, $user),
+						self::FROM_DEPT_TO_STUDENT => $this->canFromDeptToStudent(
+							$subject,
+							$user
+						),
+						self::FROM_R1_TO_STUDENT => $this->canFromR1ToStudent(
+							$subject,
+							$user
+						),
+						self::FROM_R2_TO_DEPT => $this->canFromR2ToDept($subject, $user),
+						self::FROM_R2_TO_STUDENT => $this->canFromR2ToStudent(
+							$subject,
+							$user
+						),
+						self::FROM_STUDENT_TO_R1 => $this->canFromStudentToR1(
+							$subject,
+							$user
+						),
+						self::HIDE => $this->canHide($subject, $user),
+						self::HOLD => $this->canHold($subject, $user),
+						self::PASS => $this->canPass($subject, $user),
+						self::REASSIGN => $this->canReassign($subject, $user),
+						self::RESUBMIT => $this->canResubmit($subject, $user),
+						self::SPOT_ARTICULATE => $this->canSpotArticulate($subject, $user),
+						default => false,
+				};
+		}
+
+		private function canCreate(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canRead(Evaluation $evaluation, UserInterface $user): bool {
+				return true;
+		}
+
+		private function canUpdate(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canDelete(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canAnnotate(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canAnnotateAsRequester(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canAppend(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canAssign(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canEvaluate(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canExample(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canFinalize(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canForward(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canFromCompleteToHold(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canFromDeptToR1(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canFromDeptToStudent(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canFromR1ToStudent(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canFromR2ToDept(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canFromR2ToStudent(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canFromStudentToR1(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canHide(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canHold(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canPass(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canReassign(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canResubmit(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+		private function canSpotArticulate(Evaluation $evaluation, UserInterface $user): bool {
+				return false;
+		}
+
+}
+
