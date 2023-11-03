c9484e51c62648535081f5ea71bbf2ca503bb221
David Barbero <barberod@outlook.com>
2023-11-03 14:21:25 -0400

    add rules to evaluation perm voter

--- src/Security/Voter/EvaluationVoter.php
+++ src/Security/Voter/EvaluationVoter.php
@@ -18,10 +18,13 @@ class EvaluationVoter extends Voter
 		public const CREATE = 'create';
 		public const READ = 'read';
 		public const UPDATE = 'update';
+		public const UPDATE_AS_REQUESTER = 'update_as_requester';
 		public const DELETE = 'delete';
+		public const DELETE_AS_REQUESTER = 'delete_as_requester';
 		public const ANNOTATE = 'annotate';
 		public const ANNOTATE_AS_REQUESTER = 'annotate_as_requester';
 		public const APPEND = 'append';
+		public const APPEND_AS_REQUESTER = 'append_as_requester';
 		public const ASSIGN = 'assign';
 		public const EVALUATE = 'evaluate';
 		public const EXAMPLE = 'example';
@@ -101,10 +104,13 @@ class EvaluationVoter extends Voter
 						self::CREATE => $this->canCreate($subject, $user, $attributes[0]),
 						self::READ => $this->canRead($subject, $user, $attributes[0]),
 						self::UPDATE => $this->canUpdate($subject, $user, $attributes[0]),
+						self::UPDATE_AS_REQUESTER => $this->canUpdateAsRequester($subject, $user, $attributes[0]),
 						self::DELETE => $this->canDelete($subject, $user, $attributes[0]),
+						self::DELETE_AS_REQUESTER => $this->canDeleteAsRequester($subject, $user, $attributes[0]),
 						self::ANNOTATE => $this->canAnnotate($subject, $user, $attributes[0]),
 						self::ANNOTATE_AS_REQUESTER => $this->canAnnotateAsRequester($subject, $user, $attributes[0]),
 						self::APPEND => $this->canAppend($subject, $user, $attributes[0]),
+						self::APPEND_AS_REQUESTER => $this->canAppendAsRequester($subject, $user, $attributes[0]),
 						self::ASSIGN => $this->canAssign($subject, $user, $attributes[0]),
 						self::EVALUATE => $this->canEvaluate($subject, $user, $attributes[0]),
 						self::EXAMPLE => $this->canExample($subject, $user, $attributes[0]),
@@ -132,37 +138,98 @@ class EvaluationVoter extends Voter
 		}
 
 		private function canRead(Evaluation $evaluation, UserInterface $user, string $context): bool {
-				if ($context === self::COORDINATOR) {
+				// Admin, Manager, Coordinator, Observer, Assignee can read everything
+				if (in_array($context, [self::ADMIN, self::MANAGER, self::COORDINATOR, self::OBSERVER, self::ASSIGNEE])) {
+						return true;
+				}
+				// Requester can read only their own evaluations
+				if ($context == self::REQUESTER && ($evaluation->getRequester() === $user)) {
 						return true;
 				}
 				return false;
 		}
 
 		private function canUpdate(Evaluation $evaluation, UserInterface $user, string $context): bool {
+				// Admin, Manager, Coordinator can update everything
+				if (in_array($context, [self::ADMIN, self::MANAGER, self::COORDINATOR])) {
+						return true;
+				}
+				return false;
+		}
+
+		private function canUpdateAsRequester(Evaluation $evaluation, UserInterface $user, string $context): bool {
+				// Requester can update only their own evaluations of phase "Student"
+				if ($context == self::REQUESTER && ($evaluation->getRequester() === $user) && ($evaluation->getPhase() == 'Student')) {
+						return true;
+				}
 				return false;
 		}
 
 		private function canDelete(Evaluation $evaluation, UserInterface $user, string $context): bool {
+				// Admin, Manager can delete everything
+				if (in_array($context, [self::ADMIN, self::MANAGER])) {
+						return true;
+				}
+				return false;
+		}
+
+		private function canDeleteAsRequester(Evaluation $evaluation, UserInterface $user, string $context): bool {
+				// Requester can delete only their own evaluations of phase "Student"
+				if ($context == self::REQUESTER && ($evaluation->getRequester() === $user) && ($evaluation->getPhase() == 'Student')) {
+						return true;
+				}
 				return false;
 		}
 
 		private function canAnnotate(Evaluation $evaluation, UserInterface $user, string $context): bool {
-				return true;
+				// Admin, Manager, Coordinator, Observer, Assignee can annotate everything
+				if (in_array($context, [self::ADMIN, self::MANAGER, self::COORDINATOR, self::OBSERVER, self::ASSIGNEE])) {
+						return true;
+				}
+				return false;
 		}
 
 		private function canAnnotateAsRequester(Evaluation $evaluation, UserInterface $user, string $context): bool {
+				// Requester can annotate-as-requester only their own evaluations
+				if ($context == self::REQUESTER && ($evaluation->getRequester() === $user)) {
+						return true;
+				}
 				return false;
 		}
 
 		private function canAppend(Evaluation $evaluation, UserInterface $user, string $context): bool {
-				return true;
+				// Admin, Manager, Coordinator, Observer, Assignee can append to everything
+				if (in_array($context, [self::ADMIN, self::MANAGER, self::COORDINATOR, self::OBSERVER, self::ASSIGNEE])) {
+						return true;
+				}
+				return false;
+		}
+
+		private function canAppendAsRequester(Evaluation $evaluation, UserInterface $user, string $context): bool {
+				// Requester can append-as-requester only their own evaluations
+				if ($context == self::REQUESTER && ($evaluation->getRequester() === $user)) {
+						return true;
+				}
+				return false;
 		}
 
 		private function canAssign(Evaluation $evaluation, UserInterface $user, string $context): bool {
-				return true;
+				// Admin, Manager, Coordinator can assign any evaluation with phase R1
+				if (in_array($context, [self::ADMIN, self::MANAGER, self::COORDINATOR]) && ($evaluation->getPhase() == 'Registrar 1')) {
+						return true;
+				}
+				return false;
 		}
 
 		private function canEvaluate(Evaluation $evaluation, UserInterface $user, string $context): bool {
+				// Admin, Manager, Coordinator can evaluate any evaluation with phase "Department"
+				if (in_array($context, [self::ADMIN, self::MANAGER, self::COORDINATOR]) && ($evaluation->getPhase() == 'Department')) {
+						return true;
+				}
+				// Assignee can evaluate only evaluations assigned to them with phase "Department"
+				if ($context == self::ASSIGNEE && ($evaluation->getPhase() == 'Department') && ($evaluation->getAssignee() === $user)) {
+						return true;
+				}
 				return false;
 		}
 
@@ -171,62 +238,122 @@ class EvaluationVoter extends Voter
 		}
 
 		private function canFinalize(Evaluation $evaluation, UserInterface $user, string $context): bool {
+				// Admin, Manager, Coordinator can finalize any evaluation with phase "R2"
+				if (in_array($context, [self::ADMIN, self::MANAGER, self::COORDINATOR]) && ($evaluation->getPhase() == 'Registrar 2')) {
+						return true;
+				}
 				return false;
 		}
 
 		private function canForward(Evaluation $evaluation, UserInterface $user, string $context): bool {
+				// Assignee can forward only evaluations assigned to them with phase "Department"
+				if ($context == self::ASSIGNEE && ($evaluation->getPhase() == 'Department') && ($evaluation->getAssignee() === $user)) {
+						return true;
+				}
 				return false;
 		}
 
 		private function canFromCompleteToHold(Evaluation $evaluation, UserInterface $user, string $context): bool {
+				// Admin, Manager, Coordinator can move to hold any evaluation with phase "Complete"
+				if (in_array($context, [self::ADMIN, self::MANAGER, self::COORDINATOR]) && ($evaluation->getPhase() == 'Complete')) {
+						return true;
+				}
 				return false;
 		}
 
 		private function canFromDeptToR1(Evaluation $evaluation, UserInterface $user, string $context): bool {
+				// Admin, Manager, Coordinator can move to R1 any evaluation with phase "Department"
+				if (in_array($context, [self::ADMIN, self::MANAGER, self::COORDINATOR]) && ($evaluation->getPhase() == 'Department')) {
+						return true;
+				}
 				return false;
 		}
 
 		private function canFromDeptToStudent(Evaluation $evaluation, UserInterface $user, string $context): bool {
+				// Admin, Manager, Coordinator can move to Student any evaluation with phase "Department"
+				if (in_array($context, [self::ADMIN, self::MANAGER, self::COORDINATOR]) && ($evaluation->getPhase() == 'Department')) {
+						return true;
+				}
 				return false;
 		}
 
 		private function canFromR1ToStudent(Evaluation $evaluation, UserInterface $user, string $context): bool {
+				// Admin, Manager, Coordinator can move to Student any evaluation with phase "Registrar 1"
+				if (in_array($context, [self::ADMIN, self::MANAGER, self::COORDINATOR]) && ($evaluation->getPhase() == 'Registrar 1')) {
+						return true;
+				}
 				return false;
 		}
 
 		private function canFromR2ToDept(Evaluation $evaluation, UserInterface $user, string $context): bool {
+				// Admin, Manager, Coordinator can move to Department any evaluation with phase "Registrar 2"
+				if (in_array($context, [self::ADMIN, self::MANAGER, self::COORDINATOR]) && ($evaluation->getPhase() == 'Registrar 2')) {
+						return true;
+				}
 				return false;
 		}
 
 		private function canFromR2ToStudent(Evaluation $evaluation, UserInterface $user, string $context): bool {
+				// Admin, Manager, Coordinator can move to Student any evaluation with phase "Registrar 2"
+				if (in_array($context, [self::ADMIN, self::MANAGER, self::COORDINATOR]) && ($evaluation->getPhase() == 'Registrar 2')) {
+						return true;
+				}
 				return false;
 		}
 
 		private function canFromStudentToR1(Evaluation $evaluation, UserInterface $user, string $context): bool {
+				// Admin, Manager, Coordinator can move to R1 any evaluation with phase "Student"
+				if (in_array($context, [self::ADMIN, self::MANAGER, self::COORDINATOR]) && ($evaluation->getPhase() == 'Student')) {
+						return true;
+				}
 				return false;
 		}
 
 		private function canHide(Evaluation $evaluation, UserInterface $user, string $context): bool {
+				// Coordinator can hide anything
+				if ($context == self::COORDINATOR) {
+						return true;
+				}
 				return false;
 		}
 
 		private function canHold(Evaluation $evaluation, UserInterface $user, string $context): bool {
+				// Admin, Manager, Coordinator can put hold any evaluation with phase Registrar 2
+				if (in_array($context, [self::ADMIN, self::MANAGER, self::COORDINATOR]) && ($evaluation->getPhase() == 'Registrar 2')) {
+						return true;
+				}
 				return false;
 		}
 
 		private function canPass(Evaluation $evaluation, UserInterface $user, string $context): bool {
+				// Assignee can pass only evaluations assigned to them with phase "Department"
+				if ($context == self::ASSIGNEE && ($evaluation->getPhase() == 'Department') && ($evaluation->getAssignee() === $user)) {
+						return true;
+				}
 				return false;
 		}
 
 		private function canReassign(Evaluation $evaluation, UserInterface $user, string $context): bool {
+				// Admin, Manager, Coordinator can reassign any evaluation with phase "Department"
+				if (in_array($context, [self::ADMIN, self::MANAGER, self::COORDINATOR]) && ($evaluation->getPhase() == 'Department')) {
+						return true;
+				}
 				return false;
 		}
 
 		private function canResubmit(Evaluation $evaluation, UserInterface $user, string $context): bool {
+				// Requester can resubmit only their own evaluations of phase "Student"
+				if ($context == self::REQUESTER && ($evaluation->getRequester() === $user) && ($evaluation->getPhase() == 'Student')) {
+						return true;
+				}
 				return false;
 		}
 
 		private function canSpotArticulate(Evaluation $evaluation, UserInterface $user, string $context): bool {
+				// Admin, Manager, Coordinator can spot articulate any evaluation with phase "R1"
+				if (in_array($context, [self::ADMIN, self::MANAGER, self::COORDINATOR]) && ($evaluation->getPhase() == 'Registrar 1')) {
+						return true;
+				}
 				return false;
 		}
 
