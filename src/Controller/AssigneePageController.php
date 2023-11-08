d9e265578d7cbb96a955502159b3238ef4950c07
David Barbero <barberod@outlook.com>
2023-11-08 08:58:23 -0500

    add controller and routes for assignees and requesters

---
+++ src/Controller/AssigneePageController.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Evaluation;
+use App\Repository\EvaluationRepository;
+use App\Service\EvaluationOptionsService;
+use Doctrine\ORM\EntityManagerInterface;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Pagerfanta\Pagerfanta;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('assignee')]
+class AssigneePageController extends AbstractController
+{
+		private EntityManagerInterface $entityManager;
+
+		public function __construct(
+			EntityManagerInterface $entityManager
+		) {
+				$this->entityManager = $entityManager;
+		}
+
+
+		#[Route('/secure/assignee', name: 'assignee_home')]
+		public function assignee(): Response
+		{
+				return $this->render('page/homepage.html.twig', [
+					'context' => 'assignee',
+					'page_title' => 'Transfer Credit Evaluation',
+					'prepend' => 'Assignee'
+				]);
+		}
+
+		#[Route('/secure/assignee/evaluation', name: 'assignee_evaluation_table', methods: ['GET'])]
+		public function assigneeEvaluationTable(EvaluationRepository $evaluationRepository): Response
+		{
+				$page = (isset($_GET['page']) && is_numeric($_GET['page'])) ? $_GET['page'] : 1;
+				// $orderby = ($_GET['by'] == 'updated') ? 'updated' : 'created';
+				// $direction = ($_GET['dir'] == 'asc') ? 'asc' : 'desc';
+
+				$queryBuilder = $evaluationRepository->getQB();
+				$adapter = new QueryAdapter($queryBuilder);
+				$pagerfanta = Pagerfanta::createForCurrentPageWithMaxPerPage($adapter, $page, 30);
+
+				return $this->render('evaluation/table.html.twig', [
+					'context' => 'assignee',
+					'page_title' => 'Evaluations',
+					'prepend' => 'Evaluations',
+					'pager' => $pagerfanta,
+				]);
+		}
+
+		#[Route('/secure/assignee/evaluation/{id}', name: 'assignee_evaluation_page', methods: ['GET'])]
+		#[IsGranted('assignee+read', 'evaluation')]
+		public function assigneeEvaluationPage(
+			Evaluation $evaluation,
+			EvaluationOptionsService $optionsService
+		):
+		Response {
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'assignee',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Evaluation #'.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'options' => $optionsService->getOptions('assignee', $evaluation),
+				]);
+		}
+
+		#[Route('/secure/assignee/evaluation/{id}/annotate', name: 'assignee_evaluation_annotate_form', methods: ['GET', 'POST'])]
+		#[IsGranted( 'assignee+annotate', 'evaluation' )]
+		public function assigneeEvaluationAnnotateForm(Evaluation $evaluation): Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'assignee',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Write a Note | Evaluation #'.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'annotate'
+				]);
+		}
+
+		#[Route('/secure/assignee/evaluation/{id}/evaluate', name: 'assignee_evaluation_evaluate_form', methods: ['GET', 'POST'])]
+		#[IsGranted( 'assignee+evaluate', 'evaluation' )]
+		public function assigneeEvaluationEvaluateForm(Evaluation $evaluation):
+		Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'assignee',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Enter Equivalencies | Evaluation #'
+						.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'evaluate'
+				]);
+		}
+
+		#[Route('/secure/assignee/evaluation/{id}/forward', name: 'assignee_evaluation_forward_form', methods: ['GET', 'POST'])]
+		#[IsGranted( 'assignee+forward', 'evaluation' )]
+		public function assigneeEvaluationForwardForm(Evaluation $evaluation):
+		Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'assignee',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Forward to a Colleague | Evaluation #'
+						.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'forward'
+				]);
+		}
+
+		#[Route('/secure/assignee/evaluation/{id}/pass', name: 'assignee_evaluation_pass_form', methods: ['GET', 'POST'])]
+		#[IsGranted( 'assignee+pass', 'evaluation' )]
+		public function assigneeEvaluationPassForm(Evaluation $evaluation): Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'assignee',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Pass | Evaluation #'.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'pass'
+				]);
+		}
+
+		#[Route('/secure/assignee/course', name: 'assignee_course_table', methods: ['GET'])]
+		public function assigneeCourseTable(): Response
+		{
+				return $this->render('course/table.html.twig', [
+					'context' => 'assignee',
+					'page_title' => 'Courses',
+					'prepend' => 'Courses'
+				]);
+		}
+
+		#[Route('/secure/assignee/department', name: 'assignee_department_table', methods: ['GET'])]
+		public function assigneeDepartmentTable(): Response
+		{
+				return $this->render('department/table.html.twig', [
+					'context' => 'assignee',
+					'page_title' => 'Departments',
+					'prepend' => 'Departments'
+				]);
+		}
+
+		#[Route('/secure/assignee/institution', name: 'assignee_institution_table', methods: ['GET'])]
+		public function assigneeInstitutionTable(): Response
+		{
+				return $this->render('institution/table.html.twig', [
+					'context' => 'assignee',
+					'page_title' => 'Institutions',
+					'prepend' => 'Institutions'
+				]);
+		}
+}
--- src/Controller/RequesterPageController.php
+++ src/Controller/RequesterPageController.php
@@ -3,11 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Evaluation;
-use App\Entity\Note;
-use App\Form\ScratchFormType;
 use App\Repository\EvaluationRepository;
 use App\Service\EvaluationOptionsService;
 use Doctrine\ORM\EntityManagerInterface;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,13 +24,6 @@ class RequesterPageController extends AbstractController
 				$this->entityManager = $entityManager;
 		}
 
-    #[Route('/requester/page', name: 'app_requester_page')]
-    public function index(): Response
-    {
-        return $this->render('requester_page/index.html.twig', [
-            'controller_name' => 'RequesterPageController',
-        ]);
-    }
 
 		#[Route('/secure/requester', name: 'requester_home')]
 		public function requester(): Response
@@ -42,50 +35,146 @@ class RequesterPageController extends AbstractController
 				]);
 		}
 
-		#[Route('/secure/requester/evaluation', name: 'requester_evaluation_table')]
-		public function requesterEvaluationTable(): Response
+		#[Route('/secure/requester/evaluation', name: 'requester_evaluation_table', methods: ['GET'])]
+		public function requesterEvaluationTable(EvaluationRepository $evaluationRepository): Response
 		{
+				$page = (isset($_GET['page']) && is_numeric($_GET['page'])) ? $_GET['page'] : 1;
+				// $orderby = ($_GET['by'] == 'updated') ? 'updated' : 'created';
+				// $direction = ($_GET['dir'] == 'asc') ? 'asc' : 'desc';
+
+				$queryBuilder = $evaluationRepository->getQB();
+				$adapter = new QueryAdapter($queryBuilder);
+				$pagerfanta = Pagerfanta::createForCurrentPageWithMaxPerPage($adapter, $page, 30);
+
 				return $this->render('evaluation/table.html.twig', [
 					'context' => 'requester',
 					'page_title' => 'Evaluations',
-					'prepend' => 'Evaluations'
+					'prepend' => 'Evaluations',
+					'pager' => $pagerfanta,
 				]);
 		}
 
-		#[Route('/secure/requester/evaluation/create', name: 'requester_evaluation_create_form')]
-		public function requesterEvaluationCreateForm(): Response
-		{
-				$note = new Note();
-				$form = $this->createForm(ScratchFormType::class, $note);
-				return $this->render('page/form.html.twig', [
+		#[Route('/secure/requester/evaluation/{id}', name: 'requester_evaluation_page', methods: ['GET'])]
+		#[IsGranted('requester+read', 'evaluation')]
+		public function requesterEvaluationPage(
+			Evaluation $evaluation,
+			EvaluationOptionsService $optionsService
+		):
+		Response {
+				return $this->render('evaluation/page.html.twig', [
 					'context' => 'requester',
-					'page_title' => 'Create a New Evaluation',
-					'prepend' => 'Create a New Evaluation',
-					'form' => $form,
-					'verb' => 'create'
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Evaluation #'.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'options' => $optionsService->getOptions('requester', $evaluation),
 				]);
 		}
 
-		#[Route('/secure/requester/evaluation/{uuid}', name: 'requester_evaluation_page')]
-		public function requesterEvaluationPage(string $uuid): Response
+		#[Route('/secure/requester/evaluation/{id}/update-as-requester', name: 'requester_evaluation_update_form', methods: ['GET', 'POST'])]
+		#[IsGranted( 'requester+update_as_requester', 'evaluation' )]
+		public function requesterEvaluationUpdateForm(Evaluation $evaluation): Response
 		{
 				return $this->render('evaluation/page.html.twig', [
 					'context' => 'requester',
-					'page_title' => 'Evaluation #'.$uuid,
-					'prepend' => 'Evaluation #'.$uuid,
-					'uuid' => $uuid
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Edit Details | Evaluation #'.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'update'
 				]);
 		}
 
-		#[Route('/secure/requester/evaluation/{uuid}/{verb}', name: 'requester_evaluation_form')]
-		public function requesterEvaluationForm(string $uuid, string $verb): Response
+		#[Route('/secure/requester/evaluation/{id}/delete-as-requester', name: 'requester_evaluation_delete_form', methods: ['GET', 'POST'])]
+		#[IsGranted( 'requester+delete_as_requester', 'evaluation' )]
+		public function requesterEvaluationDeleteForm(Evaluation $evaluation): Response
 		{
 				return $this->render('evaluation/page.html.twig', [
 					'context' => 'requester',
-					'page_title' => 'Evaluation #'.$uuid,
-					'prepend' => 'Evaluation #'.$uuid,
-					'uuid' => $uuid,
-					'verb' => $verb
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Delete | Evaluation #'.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'delete'
+				]);
+		}
+
+		#[Route('/secure/requester/evaluation/{id}/annotate-as-requester', name: 'requester_evaluation_annotate_as_requester_form', methods: ['GET', 'POST'])]
+		#[IsGranted( 'requester+annotate_as_requester', 'evaluation' )]
+		public function requesterEvaluationAnnotateAsRequesterForm(Evaluation $evaluation): Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'requester',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Write a Note | Evaluation #'.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'annotate-as-requester'
+				]);
+		}
+
+		#[Route('/secure/requester/evaluation/{id}/append-as-requester', name: 'requester_evaluation_append_form', methods: ['GET', 'POST'])]
+		#[IsGranted( 'requester+append_as_requester', 'evaluation' )]
+		public function requesterEvaluationAppendForm(Evaluation $evaluation):
+		Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'requester',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Upload a File | Evaluation #'.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'append'
+				]);
+		}
+
+		#[Route('/secure/requester/evaluation/{id}/resubmit', name: 'requester_evaluation_resubmit_form', methods: ['GET', 'POST'])]
+		#[IsGranted( 'requester+resubmit', 'evaluation' )]
+		public function requesterEvaluationResubmitForm(Evaluation $evaluation): Response
+		{
+				return $this->render('evaluation/page.html.twig', [
+					'context' => 'requester',
+					'page_title' => 'Evaluation #'.$evaluation->getID(),
+					'prepend' => 'Resubmit | Evaluation #'.$evaluation->getID(),
+					'evaluation' => $evaluation,
+					'id' => $evaluation->getID(),
+					'uuid' => $evaluation->getID(),
+					'verb' => 'resubmit'
+				]);
+		}
+
+		#[Route('/secure/requester/course', name: 'requester_course_table', methods: ['GET'])]
+		public function requesterCourseTable(): Response
+		{
+				return $this->render('course/table.html.twig', [
+					'context' => 'requester',
+					'page_title' => 'Courses',
+					'prepend' => 'Courses'
+				]);
+		}
+
+		#[Route('/secure/requester/department', name: 'requester_department_table', methods: ['GET'])]
+		public function requesterDepartmentTable(): Response
+		{
+				return $this->render('department/table.html.twig', [
+					'context' => 'requester',
+					'page_title' => 'Departments',
+					'prepend' => 'Departments'
+				]);
+		}
+
+		#[Route('/secure/requester/institution', name: 'requester_institution_table', methods: ['GET'])]
+		public function requesterInstitutionTable(): Response
+		{
+				return $this->render('institution/table.html.twig', [
+					'context' => 'requester',
+					'page_title' => 'Institutions',
+					'prepend' => 'Institutions'
 				]);
 		}
 }
