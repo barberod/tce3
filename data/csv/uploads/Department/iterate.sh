dc8cdc9339aba91fb1f2aacbd9a54898a2ddab81
David Barbero <dbarbero3@gatech.edu>
2024-01-08 15:12:07 -0500

    add scripts to fast get csvs fast

---
+++ data/csv/uploads/Department/iterate.sh
@@ -0,0 +1,6 @@
+# Remember to set the correct value for $TCE2
+# by doing this...
+# export TCE2=https://whatever.registrar.gatech.edu/foo/bar/
+# and be sure to have that trailing slash, too.
+
+for i in `cat _department-file-list.txt`; do wget "$TCE2${i}" --no-check-certificate; done
